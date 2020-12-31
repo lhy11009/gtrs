f8edec7822ce7152c61575b6499a789187b4dc44
Haoyuan Li <hylli@ucdavis.edu>
2020-12-30 17:40:37 -0800

    Seis.py: add plot

--- gtrs/Seis.py
+++ gtrs/Seis.py
@@ -3,7 +3,9 @@ import shutil
 import warnings
 import urllib.request
 import numpy as np
+import cartopy.crs as ccrs
 import gtrs.Utilities as Utilities
+from matplotlib import pyplot as plt
 
 class SEISCAT():
     """
@@ -14,6 +16,7 @@ class SEISCAT():
         data_float: data in format of float, to save actual data
     """
     def __init__(self):
+        self.header = []
         pass
 
     def ReadHeader(self, _filename):
@@ -29,31 +32,87 @@ class SEISCAT():
         with open(_filename, 'r') as fin:
             _texts = fin.readlines()  # read the text of the file header
         self.header = Utilities.ReadHeader2(_texts, deli=',')
+        # create self.format
+        self.GenerateFormat()
+        self.datfile = _filename
+
+    def GenerateFormat(self):
+        """
+        Generate a format array to formatting data
+        Return:
+            format of data
+        """
+        _str = ['time', 'magType', 'net', 'id', 'updated', 'place', 'type',\
+                'status', 'locationSource', 'magSource']
+        _float = ['latitude', 'longitude', 'depth', 'mag', 'nst', 'gap',\
+                   'dmin', 'rms', 'depthError', 'horizontalError', 'magError',\
+                   'magNst']
+        _format = [float for i in range(self.header['total_col'])]
+        for key, value in self.header.items():
+            if type(value) is not dict:
+                continue
+            if key in _str:
+                _format[value['col']] = "|S10"
+        # add self.format
+        self.format = _format
+        pass
     
-    def ReadData(self, _filename):
+    def ReadData(self, cols):
         '''
         Read Data
         Attributes:
             _filename(string):
                 filename for data file
         '''
-        assert(os.access(_filename, os.R_OK))  # read in data
+        # check self.datfile
+        Utilities.my_assert(self.datfile != '', AssertionError, "ReadData: Call read header first")
+        assert(os.access(self.datfile, os.R_OK))  # read in data
+
+        # if header is vacant, read header first
+        if self.header == []:
+            self.ReadHeader(_filename)
+
+        # _format = self.GenerateFormat()
+        _format = [self.format[col] for col in cols]
+       
         # import data via numpy buid in method
         # catch warning of empty file and return 1
         with warnings.catch_warnings(record=True) as w:
-            self.data_str = np.genfromtxt(_filename, skip_header=1, delimiter=',', dtype=str)
-            self.data_float = np.genfromtxt(_filename, skip_header=1, delimiter=',', dtype=float)
+            data = np.genfromtxt(self.datfile, skip_header=1, delimiter=',', dtype=_format, usecols=cols)
             if (len(w) > 0):
                 assert(issubclass(w[-1].category, UserWarning))
                 assert('Empty input file' in str(w[-1].message))
                 warnings.warn('ReadData: %s, abort' % str(w[-1].message))
                 return 1
         
-        if len(self.data_str.shape) == 1:
+        if len(data.shape) == 1:
             # only one row, expand it too 2-d array
-            self.data_str = np.array([self.data_str])
-            self.data_float = np.array([self.data_float])
-        return 0
+            data = np.array([data])
+        return data
+    
+    def Plot(self, ofile, **kwargs):
+        """
+        inputs:
+            ofile(str): output filename
+            kwargs(dict):
+                'lusi_lon': longitude
+                'lusi_lat': latitude
+        """
+        plt.figure()
+        ax = plt.axes(projection=ccrs.PlateCarree())
+        # plot earthquake epicenters
+        col_lat = self.header['latitude']['col']
+        col_lon = self.header['longitude']['col']
+        data = self.ReadData((col_lat, col_lon))
+        eq_lat = data['f0']
+        eq_lon = data['f1']
+        plt.scatter(eq_lon,eq_lat,marker='.',transform=ccrs.PlateCarree())
+        # plot central point
+        lusi_lon = kwargs.get('lusi_lon', 0.0)
+        lusi_lat = kwargs.get('lusi_lat', 0.0)
+        plt.plot(lusi_lon,lusi_lat,'r^',transform=ccrs.PlateCarree())
+        ax.coastlines(resolution='50m', color='black', linewidth=1)
+        plt.savefig(ofile)
 
 
 def url_to_usgs():
--- tests/integration/test_seis.py
+++ tests/integration/test_seis.py
@@ -28,13 +28,23 @@ def test_seiscat():
     """
     test class SEISCAT
     """
-    # initiate a class
-    Seiscat = Seis.SEISCAT()
 
     # test 1
-    ifile = os.path.join(test_source_dir, 'download_from_url_output_std.txt')
-    Seiscat.ReadHeader(ifile)
-    Seiscat.ReadData(ifile)
-    print('Seiscat.header:', Seiscat.header)
-    print('Seiscat.data_str:', Seiscat.data_str)
-    print('Seiscat.data_float:', Seiscat.data_float)
+    # download file
+    url = "https://earthquake.usgs.gov/fdsnws/event/1/query.csv?starttime=1976-05-27%2000:00:00&endtime=2006-05-28%2023:59:59&latitude=-7.526469&longitude=112.711131&maxradiuskm=1500&minmagnitude=4.0&eventtype=earthquake&orderby=time"
+    datfile = os.path.join(test_dir, 'download_from_url_output.txt')
+    # remove older file
+    if os.path.isfile(datfile):
+        os.remove(datfile)
+    Seis.download_from_url(url, datfile)
+    assert(os.access(datfile, os.R_OK))
+    # initiate a class
+    Seiscat = Seis.SEISCAT()
+    # read file
+    Seiscat.ReadHeader(datfile)
+    # plot epicenters in Solomon aera
+    ofile = os.path.join(test_dir, 'seiscat_plot.png')
+    if os.path.isfile(ofile):
+        os.remove(ofile)
+    Seiscat.Plot(ofile, lusi_lat=-7.526469, lusi_lon=112.711131)
+    assert(os.path.isfile(ofile))
